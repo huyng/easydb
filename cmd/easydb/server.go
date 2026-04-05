6aa234fd3f485a355be4a5935cf412b8fb934aca
huyng <121183+huyng@users.noreply.github.com>
2026-04-04 22:39:40 -0700

    updated docs

--- cmd/easydb/server.go
+++ cmd/easydb/server.go
@@ -149,7 +149,7 @@ func (s *Server) landingPage(w http.ResponseWriter, r *http.Request) {
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>PZDB</title>
+<title>EasyDB</title>
 <style>
   * { margin: 0; padding: 0; box-sizing: border-box; }
   body {
